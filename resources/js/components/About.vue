18c24cb9b21870c69fca49bce4a350c164fb25ea
Sayoko123f <63059711+Sayoko123f@users.noreply.github.com>
2021-06-13 19:11:49 +0800

    update aboute.vue

--- resources/js/components/About.vue
+++ resources/js/components/About.vue
@@ -18,7 +18,13 @@
           <!-- The social network for epic content -->
           110612304
         </h1>
-        <p class="mb-3 text-subtitle-1 text-sm-subtitle-1 text-md-h4 text-lg-h4 text-xl-h4 my-heading-text">
+        <p
+          class="
+            mb-3
+            text-subtitle-1 text-sm-subtitle-1 text-md-h4 text-lg-h4 text-xl-h4
+            my-heading-text
+          "
+        >
           <!-- Unleash your creativity without limitations -->
           Website Design Finial-Project
         </p>
@@ -66,251 +72,208 @@
               ><v-col>待補充</v-col></v-row
             >
             <v-row
-              ><v-col class="black--text">期望的工作地點</v-col
+              ><v-col class="black--text">期望工作地點</v-col
               ><v-col>待補充</v-col></v-row
             >
           </v-col>
         </v-row>
+        <v-row align="center">
+          <v-col
+            class="
+              text-center
+              black--text
+              text-h5 text-sm-h5 text-md-h3 text-lg-h2 text-xl-h2
+            "
+            >前端技能</v-col
+          ><v-col>
+            <v-list>
+              <v-list-item-group
+                ><v-list-item>
+                  <v-list-item-icon
+                    ><v-tooltip top
+                      ><template v-slot:activator="{ on, attrs }">
+                        <v-icon
+                          large
+                          v-on="on"
+                          v-bind="attrs"
+                          color="yellow lighten-1"
+                          >mdi-language-javascript</v-icon
+                        ></template
+                      ><span>Javascript</span>
+                    </v-tooltip></v-list-item-icon
+                  ><v-list-item-content
+                    >Javascript</v-list-item-content
+                  ></v-list-item
+                ><v-list-item
+                  ><v-list-item-icon
+                    ><v-tooltip top
+                      ><template v-slot:activator="{ on, attrs }">
+                        <v-icon
+                          large
+                          v-on="on"
+                          v-bind="attrs"
+                          color="teal accent-4"
+                          >mdi-vuejs</v-icon
+                        ></template
+                      ><span>Vue</span>
+                    </v-tooltip></v-list-item-icon
+                  ><v-list-item-content>Vue</v-list-item-content></v-list-item
+                ><v-list-item
+                  ><v-list-item-icon
+                    ><v-tooltip top
+                      ><template v-slot:activator="{ on, attrs }">
+                        <v-icon
+                          large
+                          v-on="on"
+                          v-bind="attrs"
+                          color="light-blue lighten-2"
+                          >mdi-vuetify</v-icon
+                        ></template
+                      ><span>Vuetify</span>
+                    </v-tooltip></v-list-item-icon
+                  ><v-list-item-content
+                    >Vuetify</v-list-item-content
+                  ></v-list-item
+                ></v-list-item-group
+              >
+            </v-list>
+          </v-col>
+        </v-row>
+        <v-row align="center">
+          <v-col
+            class="
+              text-center
+              black--text
+              text-h5 text-sm-h5 text-md-h3 text-lg-h2 text-xl-h2
+            "
+            >後端技能</v-col
+          ><v-col>
+            <v-list>
+              <v-list-item-group
+                ><v-list-item>
+                  <v-list-item-icon
+                    ><v-tooltip top
+                      ><template v-slot:activator="{ on, attrs }">
+                        <v-icon
+                          large
+                          v-on="on"
+                          v-bind="attrs"
+                          color="indigo lighten-3"
+                          >mdi-language-php</v-icon
+                        ></template
+                      ><span>PHP</span>
+                    </v-tooltip></v-list-item-icon
+                  ><v-list-item-content>PHP</v-list-item-content></v-list-item
+                ><v-list-item
+                  ><v-list-item-icon
+                    ><v-tooltip top
+                      ><template v-slot:activator="{ on, attrs }">
+                        <v-icon large v-on="on" v-bind="attrs" color="grey"
+                          >mdi-database</v-icon
+                        ></template
+                      ><span>MySQL</span>
+                    </v-tooltip></v-list-item-icon
+                  ><v-list-item-content>MySQL</v-list-item-content></v-list-item
+                ><v-list-item
+                  ><v-list-item-icon
+                    ><v-tooltip top
+                      ><template v-slot:activator="{ on, attrs }">
+                        <v-icon
+                          large
+                          v-on="on"
+                          v-bind="attrs"
+                          color="red accent-1"
+                          >mdi-laravel</v-icon
+                        ></template
+                      ><span>Laravel</span>
+                    </v-tooltip></v-list-item-icon
+                  ><v-list-item-content
+                    >Laravel</v-list-item-content
+                  ></v-list-item
+                ></v-list-item-group
+              >
+            </v-list>
+          </v-col>
+        </v-row>
+        <v-row align="center">
+          <v-col
+            class="
+              text-center
+              black--text
+              text-h5 text-sm-h5 text-md-h3 text-lg-h2 text-xl-h2
+            "
+            >工具</v-col
+          ><v-col>
+            <v-list>
+              <v-list-item-group
+                ><v-list-item>
+                  <v-list-item-icon
+                    ><v-tooltip top
+                      ><template v-slot:activator="{ on, attrs }">
+                        <v-icon large v-on="on" v-bind="attrs" color="red"
+                          >mdi-git</v-icon
+                        ></template
+                      ><span>Git</span>
+                    </v-tooltip></v-list-item-icon
+                  ><v-list-item-content>Git</v-list-item-content></v-list-item
+                ><v-list-item
+                  ><v-list-item-icon
+                    ><v-tooltip top
+                      ><template v-slot:activator="{ on, attrs }">
+                        <v-icon large v-on="on" v-bind="attrs" color="black"
+                          >mdi-github-circle</v-icon
+                        ></template
+                      ><span>Github</span>
+                    </v-tooltip></v-list-item-icon
+                  ><v-list-item-content
+                    >Github</v-list-item-content
+                  ></v-list-item
+                ><v-list-item
+                  ><v-list-item-icon
+                    ><v-tooltip top
+                      ><template v-slot:activator="{ on, attrs }">
+                        <v-icon large v-on="on" v-bind="attrs" color="black"
+                          >mdi-linux</v-icon
+                        ></template
+                      ><span>Linux</span>
+                    </v-tooltip></v-list-item-icon
+                  ><v-list-item-content>Linux</v-list-item-content></v-list-item
+                ></v-list-item-group
+              >
+            </v-list>
+          </v-col>
+        </v-row>
       </v-card-text>
     </v-card>
-    <v-container class="mt-2">
-      <v-row>
-        <v-col class="text-center">
-          <h2 class="text-h5 text-sm-h5 text-md-h3 text-lg-h2 text-xl-h2">
-            Front-end Skill
-          </h2>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col class="text-center">
-          <v-row>
-            <v-col>
-              <v-tooltip top
-                ><template v-slot:activator="{ on, attrs }">
-                  JS<v-icon
-                    large
-                    v-on="on"
-                    v-bind="attrs"
-                    color="yellow lighten-1"
-                    >mdi-language-javascript</v-icon
-                  > </template
-                ><span>Javascript</span>
-              </v-tooltip>
-            </v-col>
-            <v-col>
-              <v-tooltip top
-                ><template v-slot:activator="{ on, attrs }">
-                  Vue<v-icon
-                    large
-                    v-on="on"
-                    v-bind="attrs"
-                    color="teal accent-4"
-                    >mdi-vuejs</v-icon
-                  > </template
-                ><span>Vue</span>
-              </v-tooltip>
-            </v-col>
-            <v-col>
-              <v-tooltip top
-                ><template v-slot:activator="{ on, attrs }">
-                  Vuetify<v-icon
-                    large
-                    v-on="on"
-                    v-bind="attrs"
-                    color="light-blue lighten-2"
-                    >mdi-vuetify</v-icon
-                  > </template
-                ><span>Vuetify</span>
-              </v-tooltip>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col
-              ><span ref="bar1" data-bar="bar1"
-                ><v-progress-linear
-                  color="light-blue"
-                  height="10"
-                  :value="bar1"
-                  striped
-                ></v-progress-linear></span
-            ></v-col>
-            <v-col
-              ><v-progress-linear
-                color="light-blue"
-                height="10"
-                :value="bar1"
-                striped
-              ></v-progress-linear
-            ></v-col>
-            <v-col
-              ><v-progress-linear
-                color="light-blue"
-                height="10"
-                :value="bar1"
-                striped
-              ></v-progress-linear
-            ></v-col>
-          </v-row>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col class="text-center">
-          <h2 class="text-h5 text-sm-h5 text-md-h3 text-lg-h2 text-xl-h2">
-            Back-end Skill
-          </h2>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col class="text-center">
-          <v-row>
-            <v-col>
-              <v-tooltip top
-                ><template v-slot:activator="{ on, attrs }">
-                  PHP<v-icon
-                    large
-                    v-on="on"
-                    v-bind="attrs"
-                    color="indigo lighten-3"
-                    >mdi-language-php</v-icon
-                  > </template
-                ><span>PHP</span>
-              </v-tooltip>
-            </v-col>
-            <v-col>
-              <v-tooltip top
-                ><template v-slot:activator="{ on, attrs }">
-                  MySQL<v-icon large v-on="on" v-bind="attrs"
-                    >mdi-database</v-icon
-                  > </template
-                ><span>MySQL</span>
-              </v-tooltip>
-            </v-col>
-            <v-col>
-              <v-tooltip top
-                ><template v-slot:activator="{ on, attrs }">
-                  Laravel<v-icon
-                    large
-                    v-on="on"
-                    v-bind="attrs"
-                    color="red accent-1"
-                    >mdi-laravel</v-icon
-                  > </template
-                ><span>Laravel</span>
-              </v-tooltip>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col
-              ><span ref="bar2" data-bar="bar2"
-                ><v-progress-linear
-                  color="light-blue"
-                  height="10"
-                  :value="bar2"
-                  striped
-                ></v-progress-linear></span
-            ></v-col>
-            <v-col
-              ><v-progress-linear
-                color="light-blue"
-                height="10"
-                :value="bar2"
-                striped
-              ></v-progress-linear
-            ></v-col>
-            <v-col
-              ><v-progress-linear
-                color="light-blue"
-                height="10"
-                :value="bar2"
-                striped
-              ></v-progress-linear
-            ></v-col>
-          </v-row>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col class="text-center">
-          <h2 class="text-h5 text-sm-h5 text-md-h3 text-lg-h2 text-xl-h2">
-            Tools
-          </h2>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col class="text-center">
-          <v-row>
-            <v-col>
-              <v-tooltip top
-                ><template v-slot:activator="{ on, attrs }">
-                  Git<v-icon large v-on="on" v-bind="attrs" color="red"
-                    >mdi-git</v-icon
-                  > </template
-                ><span>Git</span>
-              </v-tooltip>
-            </v-col>
-            <v-col>
-              <v-tooltip top
-                ><template v-slot:activator="{ on, attrs }">
-                  Github<v-icon large v-on="on" v-bind="attrs" color="black"
-                    >mdi-github-circle</v-icon
-                  > </template
-                ><span>Github</span>
-              </v-tooltip>
-            </v-col>
-            <v-col>
-              <v-tooltip top
-                ><template v-slot:activator="{ on, attrs }">
-                  Linux<v-icon large v-on="on" v-bind="attrs"
-                    >mdi-linux</v-icon
-                  > </template
-                ><span>Linux</span>
-              </v-tooltip>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col
-              ><span ref="bar3" data-bar="bar3"
-                ><v-progress-linear
-                  color="light-blue"
-                  height="10"
-                  :value="bar3"
-                  striped
-                ></v-progress-linear></span
-            ></v-col>
-            <v-col
-              ><v-progress-linear
-                color="light-blue"
-                height="10"
-                :value="bar3"
-                striped
-              ></v-progress-linear
-            ></v-col>
-            <v-col
-              ><v-progress-linear
-                color="light-blue"
-                height="10"
-                :value="bar3"
-                striped
-              ></v-progress-linear
-            ></v-col>
-          </v-row>
-        </v-col>
-      </v-row>
-    </v-container>
+    <v-card class="mt-2">
+      <v-card-title
+        ><v-alert border="left" color="indigo" dark>
+          經歷
+        </v-alert></v-card-title
+      >
+      <v-card-text>
+        <v-alert text color="info">
+          <h3 class="text-h5">實習經驗</h3>
+          <div>
+            {{ longText(20) }}
+          </div>
+        </v-alert>
+        <v-alert text color="info">
+          <h3 class="text-h5">實習經驗</h3>
+          <div>
+            {{ longText(20) }}
+          </div>
+        </v-alert>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 <script>
 export default {
   mounted() {
     console.log("About mounted.", this.$vuetify.breakpoint.name);
-    this.createObserver(this.$refs.bar1);
-    this.createObserver(this.$refs.bar2);
-    this.createObserver(this.$refs.bar3);
   },
-  data: () => ({
-    bar1: 0,
-    bar2: 0,
-    bar3: 0,
-  }),
+  data: () => ({}),
   computed: {
     coverSrcSmall() {
       switch (this.$vuetify.breakpoint.name) {
@@ -325,21 +288,12 @@ export default {
     },
   },
   methods: {
-    createObserver(ref) {
-      let options = {
-        root: null,
-        rootMargin: "0px",
-        threshold: [1, 0],
-      };
-
-      let handleIntersect = (entries) => {
-        for (const entry of entries) {
-          this[entry.target.getAttribute("data-bar")] =
-            entry.intersectionRatio === 1 ? 100 : 0;
-        }
-      };
-      let observer = new IntersectionObserver(handleIntersect, options);
-      observer.observe(ref);
+    longText(time) {
+      let s = "";
+      while (time--) {
+        s += "Contents to be added. ";
+      }
+      return s;
     },
   },
 };
